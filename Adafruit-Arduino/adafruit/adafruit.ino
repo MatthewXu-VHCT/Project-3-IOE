25e45b7e77a4baa6a1448501b004de8f3d8cc220
MatthewXu-VHCT <75079874+MatthewXu-VHCT@users.noreply.github.com>
2021-04-17 23:04:36 -0400

    Add files via upload

---
+++ Adafruit-Arduino/adafruit/adafruit.ino
@@ -0,0 +1,120 @@
+//thinger LIBRARIES
+#include <SPI.h>
+#include <ESP8266WiFi.h>       //ESP8266 WiFi connection library
+#include <ThingerESP8266.h>    //THINGER.IO library
+
+//DHT22 LIBRARIES
+#include <DHT.h>
+#include <DHT_U.h>
+#include <Adafruit_Sensor.h>
+
+//SERVO LIBRARIES
+#include <Servo.h>
+
+//dht functions and pins
+#define DHTPIN 14
+#define DHTTYPE DHT22
+DHT dht(DHTPIN, DHTTYPE);
+
+//led pins
+#define LED1 12
+
+int distance;
+
+//servo pins
+#define servoPin 16
+Servo servo1;
+
+// Thinger.io connection parameters
+#define user "xumengh"
+#define device_Id "esp8266"
+#define device_credentials "_wABmUBI012_&++E"
+ThingerESP8266 thing(user, device_Id, device_credentials);
+
+
+
+void setup() {
+  Serial.begin(9600);
+
+  //sensor control
+  dht.begin();
+  pinMode(DHTPIN, INPUT);
+
+  //led control
+  pinMode(LED1,OUTPUT);
+
+  //servo control
+  pinMode(servoPin, OUTPUT);
+  servo1.attach(servoPin);
+  servo1.write(90);
+  
+  // Setup WiFi
+  thing.add_wifi("freeman", "cogeco388");
+  // Define the 'thing' with a name and data direction
+
+  thing["LED1"]<< digitalPin(LED1);
+
+
+  #define TRIGGER_PIN 15
+  #define ECHO_PIN 13
+  pinMode(TRIGGER_PIN, OUTPUT);
+  pinMode(ECHO_PIN, INPUT);
+
+
+
+  thing["SONIC"] >> [] (pson& out){
+      double duration, distance;
+      digitalWrite(TRIGGER_PIN, LOW);  // Get Start
+//      delayMicroseconds(2); // stable the line 
+      digitalWrite(TRIGGER_PIN, HIGH); // sending 10 us pulse
+//      delayMicroseconds(10); // delay 
+      digitalWrite(TRIGGER_PIN, LOW); // after sending pulse wating to receive signals 
+      duration = pulseIn(ECHO_PIN, HIGH); // calculating time 
+      distance = (duration/2) / 29.1; // single path 
+//      out["Distance"] = distance;
+      out = distance;
+  };
+
+
+    //sensor 
+    thing["dhtTemp"] >> [](pson& out){
+    // Add the values and the corresponding code
+//      out["celsius"] = dht.readTemperature();
+      out = dht.readTemperature();
+    };
+
+    thing["dhtHumi"] >> [](pson& out){
+//      out["humidity"] = dht.readHumidity();
+      out = dht.readHumidity();
+    };
+
+  //servo control
+    ///// stop speed
+  thing["SERVOstop"] << [] (pson & in) {
+    if (!in.is_empty()) {
+      servo1.write(90);
+    }
+    in = false;
+  };
+
+  ///// slow forward speed
+  thing ["SERVOforward"] << [] (pson & in) {
+    if (!in.is_empty()) {
+      servo1.write(110);
+    }
+    in = false;
+  };
+
+}
+
+void loop() {
+ thing.handle();
+ Serial.print("Temperature: "); 
+ Serial.println(dht.readTemperature());
+ Serial.print("Humidity: ");
+ Serial.println(dht.readHumidity());
+
+Serial.print("Distance");
+Serial.println(distance);
+ delay(500);
+}
---
+++ Adafruit-p5/thinger_feather_leds_servo_sensor__P5/sketch.js
@@ -0,0 +1,216 @@
+//var thingerUrl = ["https://api.thinger.io/v3/users/xumengh/devices/esp8266/resources/", "?authorization=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiJEYXNoYm9hcmRfcHJvamVjdF8zIiwidXNyIjoieHVtZW5naCJ9.hn9JVD772mlFSy1YdbThtDKGddhU_uM_T2NyqDGLLxw"]
+
+var thingerUrl = ["https://api.thinger.io/v3/users/xumengh/devices/esp8266/resources/", "?authorization=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiJEYXNoYm9hcmRfcHJvamVjdDNiIiwidXNyIjoieHVtZW5naCJ9.D-mwpRTMDg3qQXaQX_DYe4ktDb_czYIhDeOlxedNnXI"]
+
+//0,1,2,3,4,5,6
+//var resources = ["LED1", "LED2", "dhtTemp", "dhtHumi", "SERVOstop", "SERVOforward"];
+
+var resources = ["LED1", "SONIC", "dhtTemp", "dhtHumi", "SERVOstop", "SERVOforward"];
+
+
+var Auth = "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiJEYXNoYm9hcmRfcHJvamVjdDNiIiwidXNyIjoieHVtZW5naCJ9.D-mwpRTMDg3qQXaQX_DYe4ktDb_czYIhDeOlxedNnXI";
+
+////// variables for the 2 seperate data sets and the 2 seperate buttons
+var data1;
+var data2;
+var ledButton1;
+var ledButton2;
+var servoStop;
+var servoFor;
+var DHTsensorTem;
+var DHTsensorHum;
+
+var url = [];
+
+var disTem = text('Temperature', width - 50, 40);
+var disHum = text('Humidity', width - 50, 80);
+
+function setup() {
+    //// make the canvas whatever size you require
+    createCanvas(windowWidth, windowHeight);
+
+
+    for (let i = 0; i < resources.length; i++) {
+        url[i] = thingerUrl[0] + resources[i] + thingerUrl[1];
+    };
+
+
+    //////experimenting with p5 buttons
+    //    text('LED1', width - 100, height / 6);
+    ledButton1 = createButton('LED1')
+        .style('border', '2px solid #83EE9E')
+        .position(width / 2 - 100, height / 6)
+        .mouseClicked(ledOn1)
+        .doubleClicked(ledOff1)
+        .mouseOver(() => (ledButton1.style('background-color', "#F3F8FF")))
+        .mouseOut(() => (ledButton1.style('background-color', "#83EE9E")));
+
+
+    fill(0);
+    textSize(22);
+
+    text(url[2], width, 40);
+
+
+    text(url[3], width, 40);
+
+
+    servoStop = createButton('Stop')
+        .style('border', '2px solid #83EE9E')
+        .position(width / 2 - 100, height / 2)
+        .mouseClicked(servoStopT)
+        .doubleClicked(servoStopF)
+        .mouseOver(() => (servoStop.style('background-color', "#F3F8FF")))
+        .mouseOut(() => (servoStop.style('background-color', "#83EE9E")));
+
+    servoFor = createButton('Forward')
+        .style('border', '2px solid #83EE9E')
+        .position(width / 2 - 100, height / 2 + 120)
+        .mouseClicked(servoForT)
+        .doubleClicked(servoForF)
+        .mouseOver(() => (servoFor.style('background-color', "#F3F8FF")))
+        .mouseOut(() => (servoFor.style('background-color', "#83EE9E")));
+
+
+    ///// some p5 button styling, this affects all the buttons
+    let buttons = selectAll('button');
+    for (let i = 0; i < buttons.length; i++) {
+        buttons[i]
+            .size(200, 67)
+            .style('font-size', '32px')
+            .style('color', '#343434')
+            .style('background-color', "#83EE9E")
+            .style('text-align', 'center')
+            .style('transition-duration', '0.4s');
+    }
+
+
+}
+
+
+function draw() {
+    ////// the colour of the background
+    background("#F3F8FF");
+
+
+    console.log(url[2]);
+}
+
+////// these are the functions sending data to the switch case based
+////// on the specific mouseClicked, this doesnt feel optimizwed but it works
+function ledOn1() {
+    ////this sends a boolean state of false to the led1 json function
+    sendData('ledOn1');
+    ledButton1.style('border', '10px solid #83EE9E');
+}
+
+function ledOff1() {
+    ////this sends a boolean state of false to the led1 json function
+    sendData('ledOff1');
+    ledButton1.style('border', '2px solid #83EE9E');
+}
+
+
+function servoStopT() {
+    ////this sends a boolean state of true to the led2 json function 
+    sendData('servoStopT');
+    servoStop.style('border', '10px solid #83EE9E');
+}
+
+function servoStopF() {
+    ////this sends a boolean state of false to the led2 json function
+    sendData('servoStopF');
+    servoStop.style('border', '2px solid #83EE9E');
+}
+
+function servoForT() {
+    ////this sends a boolean state of true to the led2 json function 
+    sendData('servoForT');
+    servoFor.style('border', '10px solid #83EE9E');
+}
+
+function servoForF() {
+    ////this sends a boolean state of false to the led2 json function
+    sendData('servoForF');
+    servoFor.style('border', '2px solid #83EE9E');
+}
+
+
+function temp() {
+    sendData('temp');
+}
+
+function humi() {
+    sendData('humi');
+}
+
+
+
+function sendData(val) {
+
+    let urlData;
+    switch (val) {
+        case 'ledOn1':
+            urlData = true;
+            urli = url[0];
+            break;
+        case 'ledOff1':
+            urlData = false;
+            urli = url[0];
+            break;
+
+        case 'servoStopT':
+            urlData = true;
+            urli = url[4];
+            break;
+        case 'servoStopF':
+            urlData = false;
+            urli = url[4];
+            break;
+        case 'servoForT':
+            urlData = true;
+            urli = url[5];
+            break;
+        case 'servoForF':
+            urlData = false;
+            urli = url[5];
+            break;
+
+            //        case 'ultra'
+            //            urlData = true;
+            //            url = url[1];
+
+        case 'temp':
+            urlData = true;
+            urli = url[2];
+            DHTsensorTem = url[2];
+
+        case 'humi':
+            urlData = true;
+            urli = url[3].humidity;
+            DHTsensorHum = url[3];
+        default:
+            urlData = false;
+    }
+
+
+
+
+    ////// this function sends the data boolean state for each resource 
+    ////// to thinger.io using a json, it uses the switch case above
+    ////// to change the authorization, the specific resource address,
+    ////// and correct data type based on the case and the button pressed
+    let postData = {
+        method: "POST",
+        Headers: {
+            'Content-Type': "application/json;charset=UTF-8",
+            'Authorization': Auth,
+            'Accept': "application/json, text/plain, */*"
+        },
+
+        "in": urlData
+    };
+    httpPost(urli, 'application/json', postData, function (result) {
+        console.log(postData);
+    });
+}
